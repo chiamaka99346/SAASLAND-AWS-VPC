cc4aa50dda1f63848ebc81e1d898dc3f4b67745e
chiamaka <amakadums1990@gmail.com>
2025-11-07 02:33:22 +0100

    updated terraform

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,36 @@
-output "server_private_ip" {
-  description = "Private IP of the app server"
-  value       = aws_instance.server.private_ip
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
 }
 
-output "haproxy_public_ip" {
-  description = "Public IP of the HAProxy instance"
-  value       = aws_instance.haproxy.public_ip
+#output "instance_public_dns" {
+#  description = "The public DNS name of the EC2 instance"
+#  value       = aws_instance.app_server.public_dns_name
+#}
+
+output "security_group_id" {
+  description = "The ID of the created security group"
+  value       = aws_security_group.main_vpc_sg.id
+}
+
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = aws_vpc.main_vpc.id
+}
+
+output "ec2_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "eip_public_ip" {
+  description = "The Elastic IP allocated to the instance"
+  value       = aws_eip.nat_eip.public_ip
+}
+
+output "eip_allocation_id" {
+  description = "The Elastic IP allocation ID (useful for association/disassociation)"
+  value       = aws_eip.nat_eip.allocation_id
 }
 
 output "private_key" {
